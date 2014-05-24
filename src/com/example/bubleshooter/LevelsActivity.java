3447f07add4b63c5d5748eb50791afbc963da0c1
houssainy <mohamed.houssainy@hotmail.com>
2014-05-25 00:55:27 +0200

    before push

--- src/com/example/bubleshooter/LevelsActivity.java
+++ src/com/example/bubleshooter/LevelsActivity.java
@@ -1,7 +1,12 @@
 package com.example.bubleshooter;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LevelsActivity extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
